1acd592283e173985895a48962e7de4656925543
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-04 16:04:59 +0100

    first step of create user page

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   post "toggle_like", to: "likes#toggle_like", as: :toggle_like
   resources :comments, only: [:create, :destroy]
+  resources :users
 end
