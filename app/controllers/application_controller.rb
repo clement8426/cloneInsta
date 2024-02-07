6615b516ea4d7a229c1a6a39b10dcb90d93c4718
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-07 14:09:45 +0100

    ll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_number, :full_name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :phone_number, :full_name, :profile_picture, :bio, :private])
+
   end
 
   def after_sign_out_path_for(resource_or_scope)
---
+++ db/migrate/20240207121324_add_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.1]
+  def change
+
+
+
+
+  end
+end
